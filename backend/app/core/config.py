db690cea430d7e5e1c578838f6e89a9c489bb2dd
iapourtous <l.patrick74210@outlook.com>
2025-09-29 11:46:01 +0200

    correction des noms

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -3,7 +3,7 @@ from functools import lru_cache
 
 
 class Settings(BaseSettings):
-    mongodb_url: str = "mongodb://mongo:27017"
+    mongodb_url: str = "mongodb://mongodb-uxmpc:27017"
     database_name: str = "uxmcp"
     mcp_server_url: str = "http://api:8000/mcp"
     redis_url: str = "redis://redis:6379"
